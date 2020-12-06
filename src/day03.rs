80aa60aabbdb2b95ea34049c212d4eccf04a9dcd
Ogek <giuseppe_chiarella@hotmail.it>
2020-12-06 14:34:35 +0100

    Refactor Day 3

--- src/day03.rs
+++ src/day03.rs
@@ -2,47 +2,15 @@ use std::vec::Vec;
 
 type Forest = Vec<Vec<char>>;
 
-struct WalkIntoForest {
-    forest: Forest,
-    x: usize,
-    y: usize,
-    slope: (usize, usize),
-}
-
-impl WalkIntoForest {
-    fn new(forest: &Forest, slope: (usize, usize)) -> Self {
-        WalkIntoForest {
-            x: 0,
-            y: 0,
-            forest: forest.to_vec(),
-            slope,
-        }
-    }
-}
-
-impl Iterator for WalkIntoForest {
-    type Item = char;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.y >= self.forest.len() {
-            return None;
-        }
-        let c = Some(self.forest[self.y][self.x]);
-        self.x = (self.x + self.slope.0) % self.forest[0].len();
-        self.y += self.slope.1;
-        c
-    }
-}
-
 pub fn p1(input: &str) -> usize {
     count_trees(&parse_forest(input), (3, 1))
 }
 
 pub fn p2(input: &str) -> usize {
-    let forest = parse_forest(input);
+    let forest = &parse_forest(input);
     [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
         .iter()
-        .map(|&slope| count_trees(&forest, slope))
+        .map(|&slope| count_trees(forest, slope))
         .product()
 }
 
@@ -50,8 +18,26 @@ fn parse_forest(input: &str) -> Forest {
     input.lines().map(|l| l.chars().collect()).collect()
 }
 
+fn walk_into_forest<'a>(
+    forest: &'a Forest,
+    slope: (usize, usize),
+) -> impl Iterator<Item = char> + 'a {
+    let mut x = 0;
+    let mut y = 0;
+    std::iter::from_fn(move || {
+        if y >= forest.len() {
+            return None;
+        }
+
+        let c = Some(forest[y][x]);
+        x = (x + slope.0) % forest[0].len();
+        y += slope.1;
+        c
+    })
+}
+
 fn count_trees(forest: &Forest, slope: (usize, usize)) -> usize {
-    WalkIntoForest::new(forest, slope)
+    walk_into_forest(forest, slope)
         .filter(|c| match c {
             '#' => return true,
             '.' => return false,
