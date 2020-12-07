0915d6df1813dd5052bd6915b21ee7636a977ec2
Ogek <giuseppe_chiarella@hotmail.it>
2020-12-07 04:52:09 +0100

    Day 6

---
+++ src/day06.rs
@@ -0,0 +1,47 @@
+use std::collections::HashSet;
+
+pub fn p1(input: &str) -> usize {
+    input
+        .split("\n\n")
+        .map(|group| {
+            group
+                .lines()
+                .fold(HashSet::new(), fold_lines_to_answers_set)
+                .len()
+        })
+        .sum()
+}
+
+pub fn p2(input: &str) -> usize {
+    input
+        .split("\n\n")
+        .map(|group| {
+            group
+                .lines()
+                .map(line_to_answer_set)
+                // awaiting fold_first!
+                .fold(HashSet::from(('a'..='z').collect()), |acc, hs| {
+                    acc.intersection(&hs).cloned().collect()
+                })
+                .len()
+        })
+        .sum()
+}
+
+fn line_to_answer_set(line: &str) -> HashSet<char> {
+    line.chars().collect()
+}
+
+fn fold_lines_to_answers_set(set: HashSet<char>, line: &str) -> HashSet<char> {
+    set.union(&line_to_answer_set(line)).cloned().collect()
+}
+
+#[test]
+fn test_p1() {
+    assert_eq!(p1(include_str!("../inputs/day06.txt")), 6416);
+}
+
+#[test]
+fn test_p2() {
+    assert_eq!(p2(include_str!("../inputs/day06.txt")), 3050);
+}
