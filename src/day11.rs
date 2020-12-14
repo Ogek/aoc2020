b053b3b34f394ce6c5c4c57d4f2b158f3a4efe7d
Ogek <giuseppe_chiarella@hotmail.it>
2020-12-14 12:12:17 +0100

    Day11 p2

--- src/day11.rs
+++ src/day11.rs
@@ -1,4 +1,4 @@
-#[derive(Debug, Eq, PartialEq)]
+#[derive(Debug, Eq, PartialEq, Clone)]
 enum SeatType {
     Empty,
     Occupied,
@@ -22,7 +22,7 @@ type Coordinates = (isize, isize);
 pub fn p1(input: &str) -> usize {
     let mut seats_map = parse(input);
     loop {
-        let (new_seats_map, changes_count) = apply_rules(seats_map);
+        let (new_seats_map, changes_count) = move_people(seats_map, apply_rules_1);
         if changes_count == 0 {
             return count_occupied_seats(&new_seats_map);
         }
@@ -31,7 +31,14 @@ pub fn p1(input: &str) -> usize {
 }
 
 pub fn p2(input: &str) -> usize {
-    0
+    let mut seats_map = parse(input);
+    loop {
+        let (new_seats_map, changes_count) = move_people(seats_map, apply_rules_2);
+        if changes_count == 0 {
+            return count_occupied_seats(&new_seats_map);
+        }
+        seats_map = new_seats_map;
+    }
 }
 
 fn parse(input: &str) -> SeatsMap {
@@ -49,7 +56,10 @@ fn count_occupied_seats(seats_map: &SeatsMap) -> usize {
         .count()
 }
 
-fn apply_rules(seats_map: SeatsMap) -> (SeatsMap, usize) {
+fn move_people<F>(seats_map: SeatsMap, apply_rules: F) -> (SeatsMap, usize)
+where
+    F: Fn(&SeatsMap, &SeatType, Coordinates) -> Option<SeatType>,
+{
     let mut count_changes = 0;
     let new = seats_map
         .iter()
@@ -58,58 +68,87 @@ fn apply_rules(seats_map: SeatsMap) -> (SeatsMap, usize) {
             seats_line
                 .iter()
                 .enumerate()
-                .map(|(j, s)| match s {
-                    SeatType::Occupied => {
-                        match apply_occupied_seat_rule(&seats_map, (j as isize, i as isize)) {
-                            Some(new) => {
-                                count_changes += 1;
-                                new
-                            }
-                            _ => SeatType::Occupied,
+                .map(
+                    |(j, s)| match apply_rules(&seats_map, s, (j as isize, i as isize)) {
+                        Some(new) => {
+                            count_changes += 1;
+                            new
                         }
-                    }
-                    SeatType::Empty => {
-                        match apply_empty_seat_rule(&seats_map, (j as isize, i as isize)) {
-                            Some(new) => {
-                                count_changes += 1;
-                                new
-                            }
-                            _ => SeatType::Empty,
-                        }
-                    }
-                    SeatType::Floor => SeatType::Floor,
-                })
+                        _ => s.clone(),
+                    },
+                )
                 .collect()
         })
         .collect();
+
     (new, count_changes)
 }
 
-fn apply_occupied_seat_rule(seats_map: &SeatsMap, coordinate: Coordinates) -> Option<SeatType> {
-    if iter_adjacents(seats_map, coordinate)
-        .filter(|adja| matches!(adja, SeatType::Occupied))
-        .count()
-        >= 4
-    {
-        Some(SeatType::Empty)
-    } else {
-        None
+fn apply_rules_1(
+    seats_map: &SeatsMap,
+    seat: &SeatType,
+    coordinates: Coordinates,
+) -> Option<SeatType> {
+    match seat {
+        SeatType::Occupied => {
+            if iter_adjacents(seats_map, coordinates)
+                .filter(|adja| matches!(adja, SeatType::Occupied))
+                .count()
+                >= 4
+            {
+                Some(SeatType::Empty)
+            } else {
+                None
+            }
+        }
+        SeatType::Empty => {
+            if iter_adjacents(seats_map, coordinates)
+                .filter(|adja| matches!(adja, SeatType::Occupied))
+                .count()
+                == 0
+            {
+                Some(SeatType::Occupied)
+            } else {
+                None
+            }
+        }
+        SeatType::Floor => None,
     }
 }
 
-fn apply_empty_seat_rule(seats_map: &SeatsMap, coordinate: Coordinates) -> Option<SeatType> {
-    if iter_adjacents(seats_map, coordinate)
-        .filter(|adja| matches!(adja, SeatType::Occupied))
-        .count()
-        == 0
-    {
-        Some(SeatType::Occupied)
-    } else {
-        None
+fn apply_rules_2(
+    seats_map: &SeatsMap,
+    seat: &SeatType,
+    coordinates: Coordinates,
+) -> Option<SeatType> {
+    match seat {
+        SeatType::Occupied => {
+            if iter_visible(seats_map, coordinates)
+                .filter(|adja| matches!(adja, SeatType::Occupied))
+                .count()
+                >= 5
+            {
+                Some(SeatType::Empty)
+            } else {
+                None
+            }
+        }
+        SeatType::Empty => {
+            if iter_visible(seats_map, coordinates)
+                .filter(|adja| matches!(adja, SeatType::Occupied))
+                .count()
+                == 0
+            {
+                Some(SeatType::Occupied)
+            } else {
+                None
+            }
+        }
+        SeatType::Floor => None,
     }
 }
 
-static ADJACENTS_DEF: [Coordinates; 8] = [
+static DIRECTIONS: [Coordinates; 8] = [
     (-1, -1),
     (-1, 0),
     (-1, 1),
@@ -120,19 +159,50 @@ static ADJACENTS_DEF: [Coordinates; 8] = [
     (0, -1),
 ];
 
+fn get_seat(seats_map: &SeatsMap, coordinates: Coordinates) -> Option<&SeatType> {
+    seats_map
+        .get(coordinates.1 as usize)
+        .and_then(|line| line.get(coordinates.0 as usize))
+}
+
 fn iter_adjacents<'a>(
     seats_map: &'a SeatsMap,
     coordinates: Coordinates,
 ) -> impl Iterator<Item = &'a SeatType> + 'a {
-    ADJACENTS_DEF.iter().filter_map(move |(x, y)| {
-        seats_map
-            .get((coordinates.1 + y) as usize)
-            .and_then(|line| line.get((coordinates.0 + x) as usize))
+    DIRECTIONS
+        .iter()
+        .filter_map(move |(x, y)| get_seat(seats_map, (coordinates.0 + x, coordinates.1 + y)))
+}
+
+fn iter_visible<'a>(
+    seats_map: &'a SeatsMap,
+    coordinates: Coordinates,
+) -> impl Iterator<Item = &'a SeatType> + 'a {
+    fn find_visible(
+        seats_map: &SeatsMap,
+        direction: Coordinates,
+        target: Coordinates,
+    ) -> Option<&SeatType> {
+        match get_seat(&seats_map, target) {
+            Some(seat) => match seat {
+                SeatType::Floor => find_visible(
+                    seats_map,
+                    direction,
+                    (target.0 + direction.0, target.1 + direction.1),
+                ),
+                _ => Some(seat),
+            },
+            None => None,
+        }
+    };
+
+    DIRECTIONS.iter().filter_map(move |(x, y)| {
+        find_visible(seats_map, (*x, *y), (coordinates.0 + x, coordinates.1 + y))
     })
 }
 
 #[test]
-fn test_adjacents_border() {
+fn test_adjacents() {
     let data = "#.##.L#.##
 #L###LL.L#
 L.#.#..#..
@@ -144,10 +214,10 @@ L.#.#..#..
 #.LL###L.L
 #.#L###.##";
     let parsed = parse(data);
-    // assert_eq!(
-    //     iter_adjacents(&parsed, (0, 0)).collect::<Vec<&SeatType>>(),
-    //     vec![&SeatType::Occupied, &SeatType::Empty, &SeatType::Floor]
-    // );
+    assert_eq!(
+        iter_adjacents(&parsed, (0, 0)).collect::<Vec<&SeatType>>(),
+        vec![&SeatType::Occupied, &SeatType::Empty, &SeatType::Floor]
+    );
     assert_eq!(
         iter_adjacents(&parsed, (5, 5)).collect::<Vec<&SeatType>>(),
         vec![
@@ -162,6 +232,27 @@ L.#.#..#..
         ]
     )
 }
+#[test]
+fn test_visible() {
+    let data = ".......#.
+...#.....
+.#.......
+.........
+..#L....#
+....#....
+.........
+#........
+...#.....";
+    let parsed = parse(data);
+    assert_eq!(
+        iter_visible(&parsed, (0, 0)).collect::<Vec<&SeatType>>(),
+        vec![&SeatType::Occupied, &SeatType::Occupied]
+    );
+    assert_eq!(
+        iter_visible(&parsed, (3, 4)).collect::<Vec<&SeatType>>(),
+        vec![&SeatType::Occupied; 8]
+    );
+}
 
 #[test]
 fn test_p1() {
@@ -170,5 +261,5 @@ fn test_p1() {
 
 #[test]
 fn test_p2() {
-    //assert_eq!(p2(include_str!("../inputs/day10.txt")), 1157018619904);
+    assert_eq!(p2(include_str!("../inputs/day11.txt")), 2121);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
     println!(
         "Solution: {}",
-        aoc2020::day11::p1(include_str!("../inputs/day11.txt"))
+        aoc2020::day11::p2(include_str!("../inputs/day11.txt"))
     );
 }
